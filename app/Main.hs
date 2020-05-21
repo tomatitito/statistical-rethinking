5b3c1b943abd89a92de6d49c52f8e9526d6f20d3
Jens Kouros <jens.kouros@posteo.de>
2020-05-22 00:18:11 +0200

    Rename datatype Move to Direction

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,9 @@ module Main where
 import Lib
 import Chapter09.GoodKingMarkov
 
+x1 = rollDie
+
 main :: IO ()
-main = putStrLn "42"
+main = do 
+  putStrLn "42"
+  putStrLn $ show x1
--- src/Chapter09/GoodKingMarkov.hs
+++ src/Chapter09/GoodKingMarkov.hs
@@ -17,6 +17,12 @@ data Island =
   deriving (Eq, Show, Enum, Bounded)
 
 
+travelFrom :: Island Move -> Island
+
+rollDie' :: State StdGen Int
+rollDie' = state $ randomR (1, 10)
+
+rollDie = evalState rollDie' (mkStdGen 0)
 next :: Island -> Island
 next current
   | current == maxBound = minBound
@@ -26,13 +32,15 @@ prev :: Island -> Island
 prev current
   | current == minBound = maxBound
   | otherwise = pred current
- 
-data Move = Clockwise | Counterclockwise
+
+data Direction = Clockwise | Counterclockwise
   deriving (Eq, Show, Enum, Bounded)
-  
+
 instance Random Move where
   randomR (a, b) g =
     case randomR (fromEnum a, fromEnum b) g of
       (x, g') -> (toEnum x, g')
   random g =
     randomR (minBound, maxBound) g
+
+
