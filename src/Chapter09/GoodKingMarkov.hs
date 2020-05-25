8ae22ff49f7f395eabd541d84e7ad91d02656336
Jens Kouros <jens.kouros@posteo.de>
2020-05-25 22:11:35 +0200

    Add new function propose with type signature suited for monadic binding and rename old propose functions

--- src/Chapter09/GoodKingMarkov.hs
+++ src/Chapter09/GoodKingMarkov.hs
@@ -35,11 +35,16 @@ counterclockwise current
   | current == minBound = maxBound
   | otherwise = pred current
 
-propose :: Island -> Direction -> Island
-propose current to =
+propose' :: Island -> Direction -> Island
+propose' current to =
   case to of
     Clockwise -> clockwise current
     Counterclockwise -> counterclockwise current
+
+proposeHelperState :: Island -> Direction -> State StdGen Island
+proposeHelperState current direction = do
+  let proposal = propose' current direction
+  return proposal
 
 randomDirection' :: State StdGen Direction
 randomDirection' = state random -- (mkStdGen 0)
@@ -49,10 +54,60 @@ randomDirection = evalState randomDirection' (mkStdGen 0)
 
 moveOrStay :: Int -> Island -> Island
 moveOrStay seed current =
-   let proposal = propose current randomDirection
+   let proposal = propose' current randomDirection
        randUnifValue = evalState  (state random) (mkStdGen seed) :: Float
        probMove = fromIntegral (toInteger (fromEnum proposal) `div` fromIntegral (toInteger (fromEnum current)))
    in 
      if randUnifValue < probMove
        then proposal
        else current
+
+data MarkovData =
+ MarkovData
+ {
+  seed :: Int,
+  chain :: [Island],
+  state' :: State StdGen Island
+  }
+
+moveOrStay'' :: Island -> State StdGen Island
+moveOrStay'' current =
+  proposeHelperState current randomDirection
+
+--x = evalState (moveOrStay'' 42 Ten) (mkStdGen 43)
+
+travel :: MarkovData -> MarkovData
+travel dat =
+  let islands = chain dat
+      s = state' dat
+      current = evalState s (mkStdGen (seed dat)) -- instead: get current from chain
+      newState = moveOrStay'' current
+      (newPosition, newGen) = runState newState (mkStdGen (seed dat))
+  in MarkovData {seed = seed dat, chain = newPosition:islands, state' = newState}
+
+x = chain $ travel MarkovData {seed = 42, chain = [], state' = proposeHelperState Ten Clockwise}
+
+-- with MarkovData
+-- propose an island (or rather, a direction)
+-- decide to move or stay
+-- write decision into MarkovData
+-- update state in MarkovData
+-- recurse
+
+-- MarkovData needs to have info about recursion base case
+-- travel function is recursive
+-- logic is:
+-- propose, decide, update
+
+-- using bind :: m a -> a -> m b -> m b
+--               State Island -> propose -> State Island
+-- propose :: Island -> State StdGen Island
+-- decide :: Island -> State StdGen Island (moveOrStay)
+-- initial >>= propose
+-- proposal >>= decide
+
+propose :: Island -> State StdGen Island
+propose current = do
+  generator <- get
+  let direction = evalState randomDirection' generator
+  return $ propose' current direction
